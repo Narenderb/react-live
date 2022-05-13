dfbdc2a82daa8b19e0ced143d0958b32d75db7da
Narenderb <34243459+Narenderb@users.noreply.github.com>
2022-05-13 08:59:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Narender
+          Learn React Narender redeploy
         </a>
       </header>
     </div>
