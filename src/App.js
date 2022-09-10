b8f0a6bd1d368165be710eb150fcc484f3416b25
Narenderb <narender.baddapuri@gmail.com>
2022-09-10 22:28:57 +0530

    checking with development branch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Git deployment checking</div>
+        <div>Git deployment checking with developement</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
